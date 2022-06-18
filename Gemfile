7d4718810867df94c1c175eb4dc5c16d6a442f62
Lorenzo Galli <lrnzgll@gmail.com>
2022-06-18 12:35:01 +0100

    Comment out ziggurat-expressions

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'jwt'
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'rails', '~> 7.0.2', '>= 7.0.2.3'
-gem 'ziggurat-expressions', path: '../ziggurat-expressions'
+# gem 'ziggurat-expressions', path: '../ziggurat-expressions'
 #
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
--- config/initializers/load_expressions.rb
+++ config/initializers/load_expressions.rb
@@ -5,10 +5,9 @@ module ZigguratApi
   # If there are new expressions in the yml file, they are added to the database.
   class Application < Rails::Application
     config.after_initialize do
-      Expressions::SyncService.call(
-        expressions: YAML.safe_load(Ziggurat::Expressions::List.yml),
-        dry_run: false
-      )
+      #      Expressions::SyncService.call(
+      #        expressions: YAML.safe_load(Ziggurat::Expressions::List.yml),
+      #        dry_run: false)
     end
   end
 end
