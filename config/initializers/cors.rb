c980213098c664b4bc94151e1e979896f533720d
Lorenzo Galli <lrnzgll@gmail.com>
2022-06-19 17:39:06 +0100

    Update cors config again :-(

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.credentials[Rails.env.to_sym][:cors] || 'https://majestic-piroshki-605119.netlify.app'
+    origins ["http://localhost:8080", "https://majestic-piroshki-605119.netlify.app"]
 
     resource "*",
              headers: :any,
