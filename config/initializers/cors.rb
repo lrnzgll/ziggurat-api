43747516252bbe9152ab3d5ffc791aace40e7f84
Lorenzo Galli <lrnzgll@gmail.com>
2022-06-19 16:47:42 +0100

    Add staging env and cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8080"
+    origins Rails.application.credentials[Rails.env.to_sym][:cors]
 
     resource "*",
              headers: :any,
